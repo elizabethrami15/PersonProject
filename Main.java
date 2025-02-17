9bb30c88774ede90143410e563db16aa34446b39
elizabethrami15 <gabbyramirez2021@gmail.com>
2025-02-17 10:10:42 -0600

    Update Main.java
    Added getPopulation

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main{
     public static void main(String[] args) {
+
+        System.out.println("\nQuery Population...");
+        System.out.println("You have "+ Person.getPopulation()+" persons");
+        
         Person one = new Person();
         Person two = new Person("Mary Jones", 17); //the grey names are parameter names, 
         //just for the programmer to know how the values are typed in
@@ -19,9 +23,12 @@ public class Main{
         System.out.println("\nTesting Validator...");
         one.setAge(-150);
         System.out.println(one.getName()+" is "+one.getAge()+" years old.");
-
+ 
         Person three = new Person("Tome Jones",-75);
         System.out.println(three.getName()+" is "+three.getAge()+" years old.");
+
+        System.out.println("\nQuery Population...");
+        System.out.println("You have "+ Person.getPopulation()+" persons");
     
     }
 }
